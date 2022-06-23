9f81b0b17a706abdbf6a385ab1a267436dcc4bbb
Marwa Noomene <Noomenemarwa26@gmail.com>
2022-06-23 08:55:34 +0200

    add mps_logo

--- app/decorators.py
+++ app/decorators.py
@@ -1,7 +1,3 @@
-
-
-
-
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 
---
+++ app/tests/test_urls.py
@@ -0,0 +1,14 @@
+from django.test import SimpleTestCase
+from django.urls import resolve, reverse
+# from app.views import core,update_core,kpi
+
+
+# class TestUrls(SimpleTestCase):
+    
+#     def test_shortage_kpi_index_url(self):
+#         url = reverse('shortage_kpi')
+#         self.assertEquals(resolve(url).func, kpi)
+
+#     def test_core_index_url(self):
+#         url = reverse('core')
+#         self.assertEquals(resolve(url).func, core)
